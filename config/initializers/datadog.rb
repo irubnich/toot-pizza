f0264ee12f29bb229d506074be95172edeef1d54
Ilya Rubnich <ilya@rubni.ch>
2022-12-09 21:47:30 -0500

    instrument more

--- config/initializers/datadog.rb
+++ config/initializers/datadog.rb
@@ -2,4 +2,9 @@ require 'ddtrace'
 
 Datadog.configure do |c|
     c.tracing.instrument :rails, service_name: 'mastodon-rails'
+    c.tracing.instrument :sidekiq
+    c.tracing.instrument :pg, service_name: 'mastodon-rails-pg'
+    c.tracing.instrument :redis, service_name: 'mastodon-rails-redis'
+    c.tracing.instrument :action_mailer
+    c.tracing.instrument :aws
 end
